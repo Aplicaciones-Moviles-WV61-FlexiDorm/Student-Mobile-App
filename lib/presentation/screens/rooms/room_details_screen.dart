5fdee610ec97faf67179f6d65b93f70830fcf475
Claudio Moreno Rosales <u20191e800@upc.edu.pe>
2024-06-15 02:45:11 -0500

    feat: view rooms details

--- lib/presentation/screens/rooms/room_details_screen.dart
+++ lib/presentation/screens/rooms/room_details_screen.dart
@@ -4,118 +4,6 @@ import 'package:flexidorm_student_app/presentation/widgets/custom_elevated_butto
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-/*
-class RoomDetailsScreen extends StatelessWidget {
-  static const String name = "room_detail_screen";
-  final Room room;
-
-  const RoomDetailsScreen({super.key, required this.room});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-  appBar: AppBar(
-    leading: IconButton(
-      icon: const Icon(Icons.arrow_back),
-      onPressed: () {
-        context.go("/home");
-      },
-    ),
-  ),
-  body: Column(
-    children: [
-      // Imagen de fondo de la habitación
-      SizedBox(
-        height: 300,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: CachedNetworkImage(
-            imageUrl: room.imageUrl,
-            width: double.infinity,
-            fit: BoxFit.cover,
-            errorWidget: (context, url, error) => const Icon(Icons.error),
-          ),
-        ),
-      ),
-      // Detalles de la habitación
-      Container(
-        margin: const EdgeInsets.only(top: 20),
-        child: Card(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  room.title,
-                  style: const TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  room.description,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "S/.${room.price.toStringAsFixed(2)} por hora",
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  room.address,
-                  style: const TextStyle(
-                    fontSize: 14,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  room.nearUniversities,
-                  style: const TextStyle(
-                    fontSize: 14,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Lógica de reserva aquí
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                    child: const Text("RESERVAR"),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    ],
-  ),
-);
-  }
-}
-*/
-
 class RoomDetailsScreen extends StatelessWidget {
   static const String name = "room_detail_screen";
   final Room room;
@@ -136,20 +24,68 @@ class RoomDetailsScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.4,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                  image: CachedNetworkImageProvider(room.imageUrl),
-                  fit: BoxFit.cover,
+            Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                      image: CachedNetworkImageProvider(room.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              ),
+                Positioned(
+                  top: 10,
+                  right: 10,
+                  child: GestureDetector(
+                    onTap: () {
+                      
+                    },
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                      child: const Icon(
+                        Icons.favorite_sharp,
+                        size: 20,
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 10,
+                  right: 10,
+                  child: GestureDetector(
+                    onTap: () {
+                      
+                    },
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                      child: const Icon(
+                        Icons.map,
+                        size: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
             const SizedBox(height: 20),
             // Card de detalles
             Padding(
-              padding: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.symmetric(horizontal: 30),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -198,11 +134,12 @@ class RoomDetailsScreen extends StatelessWidget {
                     ),
                   ),
                   // Botón de reserva
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 25),
                   Center(
                     child:  CustomElevatedButton(
                       text: "RESERVAR", 
                       backgroundColor: const Color.fromARGB(255, 135, 84, 235),
+                      widthButton: 350,
                       onPressed: () { },
                     ),
                   ),
